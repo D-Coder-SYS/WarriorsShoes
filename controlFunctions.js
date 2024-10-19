b24b0628a8956268d18fd7b31e3399d37bd18fac
Lucas Araujo <dcoderltda@gmail.com>
2024-10-19 00:48:24 -0300

    correção para versão inicial do link  para whatsapp

--- controlFunctions.js
+++ controlFunctions.js
@@ -19,7 +19,9 @@ productContainers.forEach((item, i) => {
 function SendWppMessage() {
     let DestNumber = '5585996604247';
     let mensagem = 'Olá, gostaria de saber mais sobre seus serviços de desenvolvimento web. Estou interessado em criar uma página profissional e gostaria de conversar sobre detalhes e orçamento. Aguardo seu retorno!';
-    let WppUrl = 'https://wa.me/' + DestNumber + '?/text=' + encodeURIComponent(mensagem);
+    // let WppUrl = 'https://wa.me/' + DestNumber + '?/text=' + encodeURIComponent(mensagem);
+    let WppUrl = 'https://api.whatsapp.com/send?phone=' + DestNumber + '&text=' + encodeURIComponent(mensagem);
+    console.log(WppUrl)
     window.open(WppUrl, '_blank')
 }
 
