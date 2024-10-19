eaeaf11e7eccb03d00b421153f6ba23d54686b88
Lucas Araujo <dcoderltda@gmail.com>
2024-10-19 00:15:31 -0300

    Correções de posicionamento e funcionalidades do botão de WhatsApp

--- controlFunctions.js
+++ controlFunctions.js
@@ -1,18 +1,26 @@
 const productContainers = [...document.querySelectorAll('.product-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
-console.log('teste')
+const btnWpp = document.querySelector('#btn-Wpp')
+
 productContainers.forEach((item, i) => {
     let containerDimensions = item.getBoundingClientRect();
     let containerWidth = containerDimensions.width;
 
     nxtBtn[i].addEventListener('click', () => {
         item.scrollLeft += containerWidth;
-        console.log('next')
     })
 
     preBtn[i].addEventListener('click', () => {
         item.scrollLeft -= containerWidth;
-        console.log('prev')
     })
 })
+
+function SendWppMessage() {
+    let DestNumber = '5585996604247';
+    let mensagem = 'Olá, gostaria de saber mais sobre seus serviços de desenvolvimento web. Estou interessado em criar uma página profissional e gostaria de conversar sobre detalhes e orçamento. Aguardo seu retorno!';
+    let WppUrl = 'https://wa.me/' + DestNumber + '?/text=' + encodeURIComponent(mensagem);
+    window.open(WppUrl, '_blank')
+}
+
+btnWpp.addEventListener("click", SendWppMessage)
